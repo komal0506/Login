bf559cc1fc2a35eeef44ab4eec3558acc170c23b
komal0506 <159026985+komal0506@users.noreply.github.com>
2024-02-10 07:23:33 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,22 @@
-from django.urls import path
-from .import views
+"""loginsystem URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('',views.index ,name='index'),
-    path('login/',views.login, name = 'login'),
-    path('register/',views.register, name='register'),
-    path('dashboard/',views.dashboard, name='dashboard'),
-    path('signup', views.handleSignUp, name="handleSignUp"),
-    path('login', views.handeLogin, name="handleLogin"),
-    
+    path('admin/', admin.site.urls),
+    path('',include("login.urls")),
 ]
